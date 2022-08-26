642a0966132eb67ac4166e5595adb2fe8faa2a0f
mikef <mike.fullmore@propelplm.com>
2022-08-26 14:29:04 -0700

    making chagnes to support the new exports

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const propelConnect = require('@propelsoftwaresolutions/propel-sfdc-connect');
 
 // app Configuration
 const bodySize = '80mb'
 app.use(bodyParser.json({ limit: bodySize }))
 app.use(bodyParser.urlencoded({ extended: false, limit: bodySize }))
 app.use(helmet.frameguard());
-app.listen((process.env.PORT || 5000))
+app.listen((process.env.PORT || 5001))
 
 /**
  * static
  */
 app.get('/', (req, res) => {
-  res.send('Propel PIM data server is running.')
+  const logging = propelConnect.newConnection('test', 'test')
+  res.send('Propel PIM data server is running. AND connection is ' + logging.QUERY_LIST)
 })
