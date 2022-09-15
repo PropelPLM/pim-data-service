6fc8ac2291040f03ed97540fac75854e013e77a0
mikef <mike.fullmore@propelplm.com>
2022-09-15 14:07:57 -0700

    some small changes and testing

--- local/testLegacyExportProduct.js
+++ local/testLegacyExportProduct.js
@@ -3,29 +3,16 @@ const LegacyExportProduct = require('../legacy/ExportPIM')
 const treq = {
   body: {
     sessionId:
-      '00D8c0000086640!AQkAQHu0LafnNFDfkVRbOFTqcMPA2RDIrb2d7.kI5HZpHbf6F9tIhaj9NTMaQjwuNNA6czl5.tXE_V4ahGCVylHEL8SclByg',
+      '00D8c0000086640!AQkAQJklfqn0y0vCAfxvvalymHKjIKIxkA8PhFcSY_RuRapuI4giTIsIw_WF8ksItsUJ2A_PPmS0LgrOp8wfxhtRKhi7cGfF',
     namespace: '',
     instanceUrl: 'https://pim-qa.my.salesforce.com',
     hostUrl: 'pim-qa.my.salesforce.com',
     variantValuePath: null,
     variantValueIds: [
-      'a098c00000rbJJAAA2',
-      'a098c00000rbJJHAA2',
-      'a098c00000rbJJGAA2',
-      'a098c00000rbJJFAA2',
-      'a098c00000rbJJBAA2',
-      'a098c00000rbJJLAA2',
-      'a098c00000rbJJKAA2',
-      'a098c00000rbJJJAA2',
-      'a098c00000rbJJMAA2',
-      'a098c00000rbJJIAA2',
-      'a098c00000rbJJ9AAM',
-      'a098c00000rbJJEAA2',
-      'a098c00000rbJJDAA2',
-      'a098c00000rbJJCAA2',
-      'a098c00000slMfVAAU',
-      'a098c00000slMfWAAU',
-      'a098c00000skK22AAE',
+      'a098c00000t7IQeAAM',
+      'a098c00000t7IQtAAM',
+      'a098c00000t7IQYAA2',
+      'a098c00000t7IQaAAM',
     ],
     templateVersionData: null,
     recordIds: [
@@ -34,7 +21,7 @@ const treq = {
       // 'a068c00000o5C4PAAU',
       // 'a068c00000o5C4QAAU',
       // 'a068c00000o5C4RAAU',
-      'a068c00000o5C4SAAU',
+      'a068c00000o5C4nAAE',
     ],
     options: { isTemplateExport: false, isListPageExport: true },
     linkedLabels: [],
@@ -44,7 +31,7 @@ const treq = {
     isInherited: null,
     exportType: null,
     excludedLabelIds: null,
-    categoryId: 'a058c00000dK6qLAAS',
+    categoryId: 'a058c00000e9RkDAAU',
   }
 }
 
