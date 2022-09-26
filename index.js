4185c54710b5ad20a71248cb5da4796194e9d471
mikef <mike.fullmore@propelplm.com>
2022-09-26 13:57:36 -0700

    creating tests for Routes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodySize = '80mb'
 app.use(bodyParser.json({ limit: bodySize }))
 app.use(bodyParser.urlencoded({ extended: false, limit: bodySize }))
 app.use(helmet.frameguard());
-app.listen((process.env.PORT || 5001))
+module.exports = app.listen((process.env.PORT || 5001))
 
 /**
  * objects used in routes
