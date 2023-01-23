14f5f7ad682c0c4cff01ed4116fdb1cfa05a8300
mikef <mike.fullmore@propelplm.com>
2023-01-23 10:49:23 -0800

    well this should work

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodySize = '80mb';
 app.use(bodyParser.json({ limit: bodySize }));
 app.use(bodyParser.urlencoded({ extended: false, limit: bodySize }));
 app.use(helmet.frameguard());
+app.use(express.static(__dirname + '/public'));
 module.exports = app.listen(process.env.PORT || 5001);
 
 /**
