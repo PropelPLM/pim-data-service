66fcc434b0d81c8ae49d66f99f3cb2b02bc2fb0a
KimJinPang <kim.jin@propelplm.com>
2023-03-10 21:18:04 +0800

    PIM-476: updated vvIds set

--- legacy/PimRecordListHelper.js
+++ legacy/PimRecordListHelper.js
@@ -116,7 +116,10 @@ async function PimRecordListHelper(
           lowestVariantValueIds.includes(record.get('Record_ID'))
         );
       }
+      vvIds.clear();
       variantValues.forEach(value => {
+        // update variant value ids and record ids with only those relevant to lowest variants
+        vvIds.add(value.Id);
         recordIdSet.add(helper.getValue(value, 'Variant__r.Product__c'));
       });
     }
