2aab4eeeafcb0a2ba82994cab3c0d7fb19cd5df8
KimJinPang <kim.jin@propelplm.com>
2023-06-08 02:00:21 +0800

    updated with main and set call to dev

--- legacy/utils.js
+++ legacy/utils.js
@@ -430,7 +430,7 @@ async function callAsposeToExport({
     templateContentVersionId
   } = reqBody;
   const options = {
-    hostname: 'propel-document-java-staging.herokuapp.com',
+    hostname: 'propel-document-java-staging-dev.herokuapp.com',
     path: '/v2/pimTemplateExport',
     method: 'POST',
     headers: {
