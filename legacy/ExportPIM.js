559a7b615af11e4eac9d639a7d5c409bb666cbee
KimJinPang <kim.jin@propelplm.com>
2023-06-08 01:07:56 +0800

    PIM-961: fix typo

--- legacy/ExportPIM.js
+++ legacy/ExportPIM.js
@@ -138,7 +138,7 @@ function convertArrayOfObjectsToCSV(
     // eslint-disable-next-line guard-for-in
     for (let sTempkey in keys) {
       let skey = keys[sTempkey];
-      console.log('skey: ', skey);
+      skey = skey === 'Record ID' ? 'Record_ID' : skey;
       // add , [comma] after every String value,. [except first]
       if (counter > 0) {
         csvStringResult += columnDivider;
