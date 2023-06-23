a975481afe23d6754cfbd70882de92780552a678
KimJinPang <kim.jin@propelplm.com>
2023-06-23 22:01:53 +0800

    PIM-917: remove debug

--- legacy/PimStructure.js
+++ legacy/PimStructure.js
@@ -121,17 +121,6 @@ class PimStructure {
             helper.getValue(appearingValues[j], 'Attribute_Label_Type__c') ===
             DA_TYPE
           ) {
-            console.log(
-              'Prod: ',
-              helper.getValue(appearingValues[j], 'Product__c')
-            );
-            console.log(
-              'OVV: ',
-              helper.getValue(
-                appearingValues[j],
-                'Overwritten_Variant_Value__c'
-              )
-            );
             attrValValue = await parseDigitalAssetAttrVal(
               digitalAssetMap,
               attrValValue,
@@ -235,7 +224,6 @@ class PimStructure {
                       'Attribute_Label_Type__c'
                     ) === DA_TYPE
                   ) {
-                    console.log('1: ', newValue);
                     newValue = await parseDigitalAssetAttrVal(
                       digitalAssetMap,
                       newValue,
