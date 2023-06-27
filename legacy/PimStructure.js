5f27cf33e79f3de01ce0035e93e51f9cf460a399
KimJinPang <kim.jin@propelplm.com>
2023-06-27 23:56:11 +0800

    PIM-917: fix typo

--- legacy/PimStructure.js
+++ legacy/PimStructure.js
@@ -498,7 +498,7 @@ class PimStructure {
       exportRecordsColsAndAssets = {
         daDownloadDetailsList: await this.getFinalizedDaList(
           reqBody.isInherited,
-          appearingLabelIds.replace(/'/g, '').split(', '),
+          appearingLabelIds.replace(/'/g, '').split(','),
           productVariantsDaDetailsMap,
           daDownloadDetailsList,
           variantValueHierarchyMap,
