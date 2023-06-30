21df8f75ee52c5a1cd71fb17b87408eb2cef0283
danielxji6 <danielxji6@gmail.com>
2023-06-30 13:52:58 -0700

    PIM-939: PIM | Import | New import type for digital asset relationships

--- service/PimProduct.js
+++ service/PimProduct.js
@@ -5,13 +5,14 @@ class PimProduct {
    * @param {PropelLog} log
    * @param {Array} productNames
    */
-  constructor(helper, log, productNames) {
+  constructor(helper, log, productNames, skipVariant) {
     this.helper = helper
     this.log = log
     this.products = []
     this.productIdName = {}
     this.productVariantMap = new Map()
     this.productNames = productNames
+    this.skipVariant = skipVariant
   }
 
   async populate() {
@@ -23,16 +24,18 @@ class PimProduct {
       for (let product of this.products) {
         this.productIdName[product.Id] = product.Name
       }
-      this.productVariants = await this.helper.connection.queryExtend(this.helper.namespaceQuery(
-        `select Id, Name, Order__c, Product__c from Variant__c where Product__c in (${this.helper.connection.QUERY_LIST})`
-      ), Object.keys(this.productIdName).map((pId) => `'${pId}'`))
+      if (!this.skipVariant) {
+        this.productVariants = await this.helper.connection.queryExtend(this.helper.namespaceQuery(
+          `select Id, Name, Order__c, Product__c from Variant__c where Product__c in (${this.helper.connection.QUERY_LIST})`
+        ), Object.keys(this.productIdName).map((pId) => `'${pId}'`))
 
-      for (let variant of this.productVariants) {
-        const productName = this.productIdName[variant[this.helper.namespace('Product__c')]]
-        if (!this.productVariantMap.has(productName)) {
-          this.productVariantMap.set(productName, [])
+        for (let variant of this.productVariants) {
+          const productName = this.productIdName[variant[this.helper.namespace('Product__c')]]
+          if (!this.productVariantMap.has(productName)) {
+            this.productVariantMap.set(productName, [])
+          }
+          this.productVariantMap.get(productName).push(variant)
         }
-        this.productVariantMap.get(productName).push(variant)
       }
     } catch(error) {
       this.log.addToLogs([{errors: [error] }], this.helper.namespace('Product__c'))
