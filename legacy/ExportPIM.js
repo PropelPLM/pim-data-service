72c699559e7923d7e7ec7f468acec43cb9039e90
KimJinPang <kim.jin@propelplm.com>
2023-12-08 21:18:00 +0800

    PIM-1326: add POC image download and post

--- legacy/ExportPIM.js
+++ legacy/ExportPIM.js
@@ -221,22 +221,34 @@ async function sendDADownloadRequests(
   const fileContent = 'Some COntent'
   const filename = 'testImage2.png';
   const nameOnDisk = crypto.randomBytes(20).toString('hex') + filename;
-  const file = fs.createWriteStream(nameOnDisk);
-  await downloadAssets('https://d3uk1mqqf9h27x.cloudfront.net/00DHu000001IObVMAW/2a8177c6-4ea5-4dbc-b81b-474fe3aa6fcd', filename);
+  // const file = fs.createWriteStream(nameOnDisk);
+  // await downloadAssets('https://d3uk1mqqf9h27x.cloudfront.net/00DHu000001IObVMAW/2a8177c6-4ea5-4dbc-b81b-474fe3aa6fcd', filename);
   reqBody.shouldPostToUser = true;
   reqBody.communityId = null;
-    try {
-      postToChatter(filename, nameOnDisk, '', reqBody);
-    } catch (err) {
-      console.log('error: ', err);
-    }
+  https.get('https://d3uk1mqqf9h27x.cloudfront.net/00DHu000001IObVMAW/2a8177c6-4ea5-4dbc-b81b-474fe3aa6fcd', (response) => {
+    const writer = fs.createWriteStream(nameOnDisk);
+    response.pipe(writer);
+    writer.on('finish', () => {
+      console.log('Image downloaded successfully.');
+    });
+    writer.on('error', (error) => {
+      console.error('Download failed:', error.message);
+    });
+  }).on('error', (error) => {
+    console.error('Download failed:', error.message);
+  });
+
+  try {
+    postToChatter(filename, nameOnDisk, '', reqBody);
+  } catch (err) {
+    console.log('error: ', err);
   }
   // try {
   //   postAssetZipFileToChatter(filename, filename, '', reqBody);
   // } catch (err) {
   //   console.log('error: ', err);
   // }
-// }
+}
 
 async function downloadAssets(url, destination) {
   const request = require('request');
