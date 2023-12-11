615c04930a1559a1ec119860c4e5c24facf2dd4c
KimJinPang <kim.jin@propelplm.com>
2023-12-11 18:49:57 +0800

    PIM-1326: add POC image download and post

--- legacy/ExportPIM.js
+++ legacy/ExportPIM.js
@@ -250,7 +250,7 @@ async function sendDADownloadRequests(
   }).on('error', (error) => {
     console.error('Download failed:', error.message);
   });
-  file.write('fileContent.toString()');
+  file.write(fileContent.toString('base64'));
 
   reqBody.shouldPostToUser = true;
   reqBody.communityId = null;
