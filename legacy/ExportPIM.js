3752162703374a0c3e599a8a98a325f0ca7a45d0
KimJinPang <kim.jin@propelplm.com>
2023-12-12 23:32:15 +0800

    PIM-1326: fix typo

--- legacy/ExportPIM.js
+++ legacy/ExportPIM.js
@@ -228,7 +228,7 @@ async function sendDADownloadRequests(
         try {
           zipInputStream.push(fileContent);
           zipInputStream.push(null);
-          archive.append(zipInputStream, { name: zipFileNameOnDisk });
+          archive.append(zipInputStream, { name: zipFileName });
           archive.on('finish', () => {
             postToChatter(zipFileName, zipFileNameOnDisk, '', reqBody);
           });
