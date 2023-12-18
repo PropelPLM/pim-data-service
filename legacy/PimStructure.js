5e3e8c781d24dbd2d144bdaebe0ffff0c12275fd
KimJinPang <kim.jin@propelplm.com>
2023-12-19 02:16:50 +0800

    PIM-1327: add back variable assignment

--- legacy/PimStructure.js
+++ legacy/PimStructure.js
@@ -77,7 +77,7 @@ class PimStructure {
         digitalAssetMap,
         isProduct
       );
-      // daDownloadDetailsList = exportRecordsColsAndAssets?.daDownloadDetailsList;
+      daDownloadDetailsList = exportRecordsColsAndAssets?.daDownloadDetailsList;
       Object.assign(asposeInput, {
         listPageData: exportRecordsColsAndAssets?.recordsAndCols[0]
       });
