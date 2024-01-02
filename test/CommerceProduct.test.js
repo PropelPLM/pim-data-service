70e387f124e6e34e8ebeab64137a93e3004cc75c
mikef <mike.542@gmail.com>
2024-01-02 12:55:59 -0800

    PIM-1368 | making the changes needed to support attribute sets (#95)
    * making the changes needed to support attribute sets
    
    * fixing tests
    
    ---------
    
    Co-authored-by: mikef <mike.fullmore@propelplm.com>

--- test/CommerceProduct.test.js
+++ test/CommerceProduct.test.js
@@ -6,17 +6,27 @@ describe('Commerce Cloud Product Tests', () => {
     const importCom = new ImportCommerceProduct(
       {
         body: {
+          alternateCategoryId: "a09DC000001yJXkYAM",
           data: '',
           mapping: {
-            mapping: {
-              name: 'SKU',
-              title: 'Product Name',
-              category: 'Category'
-            }
-          }
+            name: 'SKU',
+            title: 'Product Name',
+            category: 'Category'
+          },
+          options: {
+            attribute_set: "Color_Size",
+            catalog: "0ZSDC000000Gs1e4AC",
+            cms_workspace: "0ZuDC000000LBc00AG",
+            entitlement: "1CeDC000000GrrS0AS",
+            pricebook_id: "01sDC000001bLYuYAM",
+            pricebook_name: "Coffee and more Price Book",
+            webstoreId: "0ZEDC000000Gs2B4AS"
+          },
+          pimProductId: "a0FDC000004ubJX2AY",
+          skipDB: true,
+          user: "test-00wfnpqokipd@example.com"
         }
-      },
-      {}
+      }
     );
 
     it('Constructor Test', () => {
