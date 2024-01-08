5cc07dbe29155a4ec7876c886079348bae38a661
KimJinPang <kim.jin@propelplm.com>
2024-01-09 04:55:20 +0800

    PIM-1121: fix typo

--- legacy/PimRecordListHelper.js
+++ legacy/PimRecordListHelper.js
@@ -634,6 +634,7 @@ async function addExportColumns(
     if (!isProduct) {
       // check if "System Attributes" attribute group is selected and slate those for export
       hasDefaultAssetCols = await checkForDefaultAssetCols(exportColumns, linkedGroups);
+      console.log('hasDefaultAssetCols: ', hasDefaultAssetCols)
     }
   }
   if (columnAttributeIds.size > 0 || hasDefaultAssetCols) {
